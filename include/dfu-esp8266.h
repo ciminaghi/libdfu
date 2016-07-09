654d8dfcbf3bc5c49b7be07da642093203e4f095
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-07-09 07:44:06 +0200

    samples/esp8266-stm32.c: first draft of actual code

---
+++ include/dfu-esp8266.h
@@ -0,0 +1,11 @@
+#ifndef __DFU_ESP8266_H__
+#define __DFU_ESP8266_H__
+
+extern struct dfu_interface_ops esp8266_serial_interface_ops;
+extern struct dfu_host_ops esp8266_dfu_host_ops;
+
+#include <osapi.h>
+#include "espmissingincludes.h"
+
+#endif /* __DFU_ESP8266_H__ */
+
--- samples/esp8266-stm32.c
+++ samples/esp8266-stm32.c
@@ -9,6 +9,9 @@
 #include "os_type.h"
 #include "user_interface.h"
 #include "espmissingincludes.h"
+#include <dfu.h>
+#include <dfu-esp8266.h>
+#include <dfu-stm32.h>
 
 #define UART0 0
 
@@ -18,14 +21,22 @@
 os_event_t user_proc_task_queue[USER_QUEUE_LEN];
 static void loop(os_event_t *events);
 
+/*
+ * mmmmh, don't like this, but I don't know how to pass pointers to
+ * user's event handlers
+ */
+static struct dfu_data *dfu;
+static struct dfu_binary_file *bfile;
 
 static void ICACHE_FLASH_ATTR
 loop(os_event_t *events)
 {
-	static unsigned long i = 0;
-
-	os_printf("Hello %lu\n\r", i++);
-	os_delay_us(1000000);
+	if (dfu_binary_file_written(bfile)) {
+		/* File was written, start target and die */
+		dfu_target_go(dfu);
+		return;
+	}
+	dfu_idle(dfu);
 	system_os_post(USER_PRIO, 0, 0 );
 }
 
@@ -35,7 +46,24 @@ user_init()
 {
 	uart_div_modify(UART0, UART_CLK_FREQ / 115200);
 
-	/* Start os task */
+	dfu = dfu_init(&esp8266_serial_interface_ops, "", NULL,
+		       &stm32_dfu_target_ops,
+		       &esp8266_dfu_host_ops);
+
+	if (!dfu) {
+		os_printf("Error setting up dfu\n");
+		return;
+	}
+	if (dfu_target_reset(dfu) < 0) {
+		os_printf("Error resetting target\n");
+		return;
+	}
+	bfile = dfu_binary_file_start_rx("http_post", dfu);
+	if (!bfile) {
+		os_printf("Error starting file rx\n");
+		return;
+	}
+	/* Start dfu task */
 	system_os_task(loop, USER_PRIO, user_proc_task_queue, USER_QUEUE_LEN);
 	system_os_post(USER_PRIO, 0, 0 );
 }
