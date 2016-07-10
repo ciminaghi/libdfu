b5efe48299492354bafb20b48a017edc91e04c97
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-07-10 18:35:47 +0200

    target: implement reset and go

--- src/target.c
+++ src/target.c
@@ -5,10 +5,14 @@
 
 int dfu_target_reset(struct dfu_data *dfu)
 {
-	return -1;
+	if (dfu->target->ops->reset_and_sync)
+		return dfu->target->ops->reset_and_sync(dfu->target);
+	return 0;
 }
 
 int dfu_target_go(struct dfu_data *dfu)
 {
+	if (dfu->target->ops->run)
+		return dfu->target->ops->run(dfu->target);
 	return -1;
 }
