7ff1c2212a502c08ef83da9913756615b4f77edc
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-08-02 22:06:29 +0200

    improve linux-serial interface implementation

--- src/interface/linux-serial.c
+++ src/interface/linux-serial.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <poll.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 #include "dfu.h"
 #include "dfu-internal.h"
 
@@ -18,10 +23,27 @@ static struct linux_serial_data sdata;
 static int linux_serial_open(struct dfu_interface *iface,
 			     const char *path, const void *pars)
 {
+	struct termios config;
+
 	iface->priv = &sdata;
-	sdata.fd = open(path, O_RDWR);
+	sdata.fd = open(path, O_RDWR | O_NOCTTY);
 	if (sdata.fd < 0)
 		return sdata.fd;
+	/* FIXME: USE pars FOR SERIAL PORT CONFIGURATION ? */
+	if (tcgetattr(sdata.fd, &config) < 0) {
+		dfu_err("Error reading termios config\n");
+		return -1;
+	}
+	config.c_cflag |= PARENB;
+	config.c_cflag |= CS8;
+	if(cfsetispeed(&config, B115200) < 0) {
+		dfu_err("Error setting serial port speed\n");
+		return -1;
+	}
+	if (tcsetattr(sdata.fd, TCSAFLUSH, &config) < 0) {
+		dfu_err("Error setting termios config\n");
+		return -1;
+	}
 	return 0;
 }
 
@@ -39,13 +61,59 @@ int linux_serial_read(struct dfu_interface *iface, char *buf,
 		      unsigned long size)
 {
 	struct linux_serial_data *priv = iface->priv;
+	struct pollfd pfd = {
+		.fd = priv->fd,
+		.events = POLLIN,
+		.revents = 0,
+	};
+	int stat;
 
+	/* FIXME: make timeout configurable at runtime */
+	stat = poll(&pfd, 1, 1000);
+	if (stat < 0) {
+		dfu_err("%s: %s\n", __func__, strerror(errno));
+		return -1;
+	}
+	if (!stat) {
+		dfu_err("Timeout waiting for serial read\n");
+		return -1;
+	}
+	if (stat != 1) {
+		dfu_err("%s: unexpected return value from poll()\n", __func__);
+		return -1;
+	}
 	return read(priv->fd, buf, size);
 }
 
-/* FIXME: do hw reset for target (maybe gpio ?) */
+/* FIXME: THIS IS STM32 SPECIFIC, IMPLEMENT AN STM32 SPECIFIC INTERFACE */
 int linux_serial_target_reset(struct dfu_interface *iface)
 {
+	struct linux_serial_data *priv = iface->priv;
+	int v, stat;
+
+	/* RTS -> BOOT0 */
+	/* DTR -> RST */
+	/* Set RST to 0 (active) */
+	v = TIOCM_RTS;
+	stat = ioctl(priv->fd, TIOCMBIS, &v);
+	if (stat < 0) {
+		dfu_err("error resetting target (%s)\n", strerror(errno));
+		return stat;
+	}
+	v = TIOCM_RTS;
+	stat = ioctl(priv->fd, TIOCMBIC, &v);
+	if (stat < 0) {
+		dfu_err("error resetting target (%s)\n", strerror(errno));
+		return stat;
+	}
+	v = TIOCM_DTR;
+	stat = ioctl(priv->fd, TIOCMBIC, &v);
+	if (stat < 0) {
+		dfu_err("error resetting target (%s)\n", strerror(errno));
+		return stat;
+	}
+	/* Wait 50ms */
+	poll(NULL, 0, 200);
 	return 0;
 }
 
