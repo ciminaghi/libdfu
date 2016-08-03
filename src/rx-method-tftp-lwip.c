d7fea931a3181a174ceeefd7565b3d8d575f8d90
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-08-03 18:27:56 +0200

    add empty skeleton for rx-method-tftp-lwip

---
+++ src/rx-method-tftp-lwip.c
@@ -0,0 +1,21 @@
+/* This rx method uses tftp and lwip raw (no sockets) */
+
+#include "dfu.h"
+#include "dfu-internal.h"
+
+static int dfu_rx_tftp_init(struct dfu_binary_file *bf, void *arg)
+{
+	return -1;
+}
+
+static int dfu_rx_tftp_poll_idle(struct dfu_binary_file *bf)
+{
+	return 0;
+}
+
+static const struct dfu_file_rx_method_ops tftp_rx_ops = {
+	.init = dfu_rx_tftp_init,
+	.poll_idle = dfu_rx_tftp_poll_idle,
+};
+
+declare_file_rx_method(tftp, &tftp_rx_ops);
