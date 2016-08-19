184210a5b4d566345d5828ff948883d9a5a4bac5
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-08-19 07:40:35 +0200

    fix phr_parse_headers() for indentation

--- src/picohttpparser.c
+++ src/picohttpparser.c
@@ -502,25 +502,28 @@ int phr_parse_response(const char *buf_start, size_t len, int *minor_version,
 	return (int)(buf - buf_start);
 }
 
-int phr_parse_headers(const char *buf_start, size_t len, struct phr_header *headers, size_t *num_headers, size_t last_len)
+int phr_parse_headers(const char *buf_start, size_t len,
+		      struct phr_header *headers, size_t *num_headers,
+		      size_t last_len)
 {
-    const char *buf = buf_start, *buf_end = buf + len;
-    size_t max_headers = *num_headers;
-    int r;
+	const char *buf = buf_start, *buf_end = buf + len;
+	size_t max_headers = *num_headers;
+	int r;
 
-    *num_headers = 0;
+	*num_headers = 0;
 
-    /* if last_len != 0, check if the response is complete (a fast countermeasure
-       against slowloris */
-    if (last_len != 0 && is_complete(buf, buf_end, last_len, &r) == NULL) {
-        return r;
-    }
+	/*
+	 * if last_len != 0, check if the response is complete
+	 * (a fast countermeasure against slowloris
+	 */
+	if (last_len != 0 && !is_complete(buf, buf_end, last_len, &r))
+		return r;
 
-    if ((buf = parse_headers(buf, buf_end, headers, num_headers, max_headers, &r)) == NULL) {
-        return r;
-    }
+	if (!(buf = parse_headers(buf, buf_end, headers, num_headers,
+				  max_headers, &r)))
+		return r;
 
-    return (int)(buf - buf_start);
+	return (int)(buf - buf_start);
 }
 
 enum {
