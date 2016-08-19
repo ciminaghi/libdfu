87e2ee0a194ea4f001973c3d48ffe57af30a96d7
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-08-19 07:36:06 +0200

    fix phr_parse_request() for indentation

--- src/picohttpparser.c
+++ src/picohttpparser.c
@@ -410,32 +410,36 @@ static const char *parse_request(const char *buf, const char *buf_end,
 			     ret);
 }
 
-int phr_parse_request(const char *buf_start, size_t len, const char **method, size_t *method_len, const char **path,
-                      size_t *path_len, int *minor_version, struct phr_header *headers, size_t *num_headers, size_t last_len)
+int phr_parse_request(const char *buf_start, size_t len, const char **method,
+		      size_t *method_len, const char **path,
+		      size_t *path_len, int *minor_version,
+		      struct phr_header *headers, size_t *num_headers,
+		      size_t last_len)
 {
-    const char *buf = buf_start, *buf_end = buf_start + len;
-    size_t max_headers = *num_headers;
-    int r;
+	const char *buf = buf_start, *buf_end = buf_start + len;
+	size_t max_headers = *num_headers;
+	int r;
 
-    *method = NULL;
-    *method_len = 0;
-    *path = NULL;
-    *path_len = 0;
-    *minor_version = -1;
-    *num_headers = 0;
+	*method = NULL;
+	*method_len = 0;
+	*path = NULL;
+	*path_len = 0;
+	*minor_version = -1;
+	*num_headers = 0;
 
-    /* if last_len != 0, check if the request is complete (a fast countermeasure
-       againt slowloris */
-    if (last_len != 0 && is_complete(buf, buf_end, last_len, &r) == NULL) {
-        return r;
-    }
+	/*
+	 * if last_len != 0, check if the request is complete
+	 * (a fast countermeasure againt slowloris
+	 */
+	if (last_len != 0 && !is_complete(buf, buf_end, last_len, &r))
+		return r;
 
-    if ((buf = parse_request(buf, buf_end, method, method_len, path, path_len, minor_version, headers, num_headers, max_headers,
-                             &r)) == NULL) {
-        return r;
-    }
+	if (!(buf = parse_request(buf, buf_end, method, method_len, path,
+				  path_len, minor_version, headers,
+				  num_headers, max_headers, &r)))
+		return r;
 
-    return (int)(buf - buf_start);
+	return (int)(buf - buf_start);
 }
 
 static const char *parse_response(const char *buf, const char *buf_end, int *minor_version, int *status, const char **msg,
