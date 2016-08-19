6ba90bf241d3ea6d2dc11b63bcb4d54484cb596f
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-08-19 07:29:06 +0200

    picohttpparser.c: fix messy get_token_to_eol() for indentation

--- src/picohttpparser.c
+++ src/picohttpparser.c
@@ -147,73 +147,80 @@ static inline const void *advance_token(const char **tok, size_t *toklen,
 }
 
 
-static const char *get_token_to_eol(const char *buf, const char *buf_end, const char **token, size_t *token_len, int *ret)
+static const char *get_token_to_eol(const char *buf, const char *buf_end,
+				    const char **token, size_t *token_len,
+				    int *ret)
 {
-    const char *token_start = buf;
+	const char *token_start = buf;
 
 #ifdef __SSE4_2__
-    static const char ranges1[] = "\0\010"
-                                  /* allow HT */
-                                  "\012\037"
-                                  /* allow SP and up to but not including DEL */
-                                  "\177\177"
-        /* allow chars w. MSB set */
-        ;
-    int found;
-    buf = findchar_fast(buf, buf_end, ranges1, sizeof(ranges1) - 1, &found);
-    if (found)
-        goto FOUND_CTL;
+	static const char ranges1[] = "\0\010"
+		/* allow HT */
+		"\012\037"
+		/* allow SP and up to but not including DEL */
+		"\177\177"
+		/* allow chars w. MSB set */
+		;
+	int found;
+	buf = findchar_fast(buf, buf_end, ranges1, sizeof(ranges1) - 1, &found);
+	if (found)
+		goto FOUND_CTL;
 #else
-    /* find non-printable char within the next 8 bytes, this is the hottest code; manually inlined */
-    while (likely(buf_end - buf >= 8)) {
-#define DOIT()                                                                                                                     \
-    do {                                                                                                                           \
-        if (unlikely(!IS_PRINTABLE_ASCII(*buf)))                                                                                   \
-            goto NonPrintable;                                                                                                     \
-        ++buf;                                                                                                                     \
-    } while (0)
-        DOIT();
-        DOIT();
-        DOIT();
-        DOIT();
-        DOIT();
-        DOIT();
-        DOIT();
-        DOIT();
+	/*
+	 * find non-printable char within the next 8 bytes, this is the
+	 * hottest code; manually inlined
+	 */
+	while (likely(buf_end - buf >= 8)) {
+#define DOIT()								\
+		do {							\
+			if (unlikely(!IS_PRINTABLE_ASCII(*buf)))	\
+				goto NonPrintable;			\
+			++buf;						\
+		} while (0)
+		DOIT();
+		DOIT();
+		DOIT();
+		DOIT();
+		DOIT();
+		DOIT();
+		DOIT();
+		DOIT();
 #undef DOIT
-        continue;
-    NonPrintable:
-        if ((likely((unsigned char)*buf < '\040') && likely(*buf != '\011')) || unlikely(*buf == '\177')) {
-            goto FOUND_CTL;
-        }
-        ++buf;
-    }
+		continue;
+	NonPrintable:
+		if ((likely((unsigned char)*buf < '\040') &&
+		     likely(*buf != '\011')) || unlikely(*buf == '\177')) {
+			goto FOUND_CTL;
+		}
+		++buf;
+	}
 #endif
-    for (;; ++buf) {
-	    if (!check_eof(buf, buf_end, ret))
-		    return NULL;
-        if (unlikely(!IS_PRINTABLE_ASCII(*buf))) {
-            if ((likely((unsigned char)*buf < '\040') && likely(*buf != '\011')) || unlikely(*buf == '\177')) {
-                goto FOUND_CTL;
-            }
-        }
-    }
+	for (;; ++buf) {
+		if (!check_eof(buf, buf_end, ret))
+			return NULL;
+		if (unlikely(!IS_PRINTABLE_ASCII(*buf))) {
+			if ((likely((unsigned char)*buf < '\040') &&
+			     likely(*buf != '\011')) ||
+			    unlikely(*buf == '\177')) {
+				goto FOUND_CTL;
+			}
+		}
+	}
 FOUND_CTL:
-    if (likely(*buf == '\015')) {
-        ++buf;
-        if (!expect_char('\012', buf, buf_end, ret))
+	if (likely(*buf == '\015')) {
+		++buf;
+		if (!expect_char('\012', buf, buf_end, ret))
+			return NULL;
+		*token_len = buf - 2 - token_start;
+	} else if (*buf == '\012') {
+		*token_len = buf - token_start;
+		++buf;
+	} else {
+		*ret = -1;
 		return NULL;
-        *token_len = buf - 2 - token_start;
-    } else if (*buf == '\012') {
-        *token_len = buf - token_start;
-        ++buf;
-    } else {
-        *ret = -1;
-        return NULL;
-    }
-    *token = token_start;
-
-    return buf;
+	}
+	*token = token_start;
+	return buf;
 }
 
 static const char *is_complete(const char *buf, const char *buf_end, size_t last_len, int *ret)
