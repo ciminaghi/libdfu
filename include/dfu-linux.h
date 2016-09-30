4286879bd669840925b1dd145398702d19c4a061
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-09-30 16:48:49 +0200

    interface: add linux serial for arduino board

--- include/dfu-linux.h
+++ include/dfu-linux.h
@@ -15,6 +15,7 @@ struct linux_event_data {
 };
 
 extern const struct dfu_interface_ops linux_serial_stm32_interface_ops;
+extern const struct dfu_interface_ops linux_serial_arduino_uno_interface_ops;
 extern const struct dfu_host_ops linux_dfu_host_ops;
 
 #define dfu_log(a,args...) fprintf(stderr, "DFU: " a, ##args)
