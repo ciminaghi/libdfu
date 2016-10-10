a0ad41f5f544c549cb143ef2ddbca8f104d691a0
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-10-10 14:49:03 +0200

    first draft of esp8266 host implementation

--- src/host/esp8266.c
+++ src/host/esp8266.c
@@ -3,7 +3,33 @@
 #include "dfu.h"
 #include "dfu-esp8266.h"
 #include "dfu-internal.h"
+#include "user_interface.h"
 
+static int esp8266_init(struct dfu_host *host)
+{
+	uart_div_modify(0, UART_CLK_FREQ / 115200);
+	dfu_dbg("%s\n", __func__);
+	return 0;
+}
+
+void esp8266_udelay(struct dfu_host *host, unsigned long us)
+{
+	os_delay_us(us);
+}
+
+int esp8266_idle(struct dfu_host *host, int timeout_ms)
+{
+	return DFU_ERROR;
+}
+
+unsigned long esp8266_get_current_time(struct dfu_host *host)
+{
+	return system_get_time();
+}
 
 const struct dfu_host_ops esp8266_dfu_host_ops = {
+	.init = esp8266_init,
+	.udelay = esp8266_udelay,
+	.idle = esp8266_idle,
+	.get_current_time = esp8266_get_current_time,
 };
