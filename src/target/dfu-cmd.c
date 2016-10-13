e06f68733d34e621e55aeb256315f169cd558179
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-10-13 15:13:59 +0200

    fix: target/dfu-cmd: dfu_cmd_start(): just run first cmd buffer
    To keep things simpler, we just try to run the first command's buffer and
    return.

--- src/target/dfu-cmd.c
+++ src/target/dfu-cmd.c
@@ -199,11 +199,9 @@ int dfu_cmd_start(struct dfu_target *target, const struct dfu_cmddescr *descr)
 	dfu_target_set_busy(target);
 	state->status = DFU_CMD_STATUS_INITIALIZED;
 	state->cmdbuf_index = 0;
-	do {
-	    stat = _do_cmdbuf(target, descr,
-			      &descr->cmdbufs[state->cmdbuf_index]);
-	    dfu_dbg("%s: _do_cmdbuf returns %d\n", __func__, stat);
-	} while(stat == DO_CMDBUF_CONTINUE);
+	stat = _do_cmdbuf(target, descr,
+			  &descr->cmdbufs[state->cmdbuf_index]);
+	dfu_dbg("%s: _do_cmdbuf returns %d\n", __func__, stat);
 
 	return stat == DO_CMDBUF_ERROR ? -1 : 0;
 }
