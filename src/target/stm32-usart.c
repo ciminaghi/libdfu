a5efb098afc0012c75fc5ef55419548dc7d370d4
Davide Ciminaghi <ciminaghi@gnudd.com>
2017-01-25 14:51:30 +0100

    target, stm32-usart: use dfu-cmd infrastructure

--- src/target/stm32-usart.c
+++ src/target/stm32-usart.c
@@ -7,37 +7,18 @@
 
 #include "dfu.h"
 #include "dfu-internal.h"
+#include "dfu-cmd.h"
 
+#define ACK 0x79
+
+struct stm32_usart_data {
 #define STM32_EXTENDED_MEMORY_ERASE	(1 << 0)
 #define STM32_DOUBLE_NAK		(1 << 1)
-
-static int target_flags;
-
-enum stm32_usart_dir {
-	IN,
-	OUT,
-};
-
-/*
- * Send flags
- */
-#define START_CHECKSUM 1
-#define SEND_CHECKSUM  2
-#define NO_ACK         4
-
-struct stm32_usart_cmdbuf {
-	enum stm32_usart_dir dir;
-	int flags;
-	union {
-		void *in;
-		const void *out;
-	} buf;
-	unsigned int len;
-};
-
-struct stm32_usart_cmdescr {
-	struct stm32_usart_cmdbuf *cmdbufs;
-	int ncmdbufs;
+	int target_flags;
+	phys_addr_t curr_chunk_addr;
+	struct dfu_cmdstate cmd_state;
+	struct dfu_timeout cmd_timeout;
+	const struct dfu_cmddescr *curr_descr;
 };
 
 struct stm32_get_cmd_reply {
@@ -53,134 +34,120 @@ struct stm32_gid_cmd_reply {
 	uint8_t ack;
 };
 
-static int wait_for_ack(struct dfu_interface *interface)
+
+static void checksum_update(const struct dfu_cmddescr *descr, const void *_buf,
+			    unsigned int l)
 {
-	char c;
-	int stat = interface->ops->read(interface, &c, 1);
+	uint8_t *cptr = descr->checksum_ptr;
+	const uint8_t *buf = _buf;
+	unsigned int i;
 
-	if (stat < 0)
-		return -1;
-	return c == 0x79 ? 0 : -1;
-}
-
-static void checksum_update(const uint8_t *buf, unsigned int l, uint8_t *out)
-{
-	int i;
-
+	if (!cptr) {
+		dfu_dbg("%s invoked with NULL checksum ptr\n", __func__);
+		return;
+	}
 	for (i = 0; i < l; i++)
-		*out ^= buf[i];
+		*cptr ^= buf[i];
 }
 
-int send_cmd(struct dfu_target *target,
-	     const struct stm32_usart_cmdescr *descr)
+static int _check_ack(const struct dfu_cmddescr *descr,
+		      const struct dfu_cmdbuf *buf)
 {
-	struct dfu_interface *interface = target->interface;
-	struct stm32_usart_cmdbuf *ptr;
-	int i, stat;
-	static uint8_t checksum;
+	char *ptr = buf->buf.in;
 
-	if (!interface || !interface->ops || !interface->ops->write) {
-		dfu_err("%s: cannot write to interface\n", __func__);
-		return -1;
-	}
-	for (i = 0, ptr = descr->cmdbufs; i < descr->ncmdbufs; i++, ptr++) {
-		if (ptr->flags & START_CHECKSUM)
-			checksum = 0;
-		switch (ptr->dir) {
-		case OUT:
-			checksum_update(ptr->buf.out, ptr->len, &checksum);
-			stat = interface->ops->write(interface, ptr->buf.out,
-						     ptr->len);
-			if (stat < ptr->len) {
-				dfu_err("%s: error writing to interface\n",
-					__func__);
-				return -1;
-			}
-			if (ptr->flags & SEND_CHECKSUM)
-				stat = interface->ops->write(interface,
-							     (char *)&checksum,
-							     sizeof(checksum));
-			if (stat < 1) {
-				dfu_err("%s: error sending checksum\n",
-					__func__);
-				return -1;
-			}
-			if (!(ptr->flags & NO_ACK))
-				if (wait_for_ack(interface) < 0) {
-					dfu_err("%s: no ACK or NAK\n",
-						__func__);
-					return -1;
-				}
-			break;
-		case IN:
-			stat = interface->ops->read(interface, ptr->buf.in,
-						     ptr->len);
-			if (stat < ptr->len) {
-				dfu_err("%s: error reading from interface\n",
-					__func__);
-				return -1;
-			}
-			break;
-		default:
-			dfu_err("%s: invalid buffer direction\n", __func__);
-			return -1;
-		}
-	}
-	return 0;
+	dfu_dbg("%s: ptr[0] = 0x%02x (expected 0x%02x)\n", __func__, ptr[0],
+		ACK);
+	return ptr[0] == ACK ? 0 : -1;
 }
 
 static int get_cmd(struct dfu_target *target, struct stm32_get_cmd_reply *r)
 {
+	struct stm32_usart_data *priv = target->priv;
 	static const uint8_t cmdb[] = { 0, 0xff, };
-	static struct stm32_usart_cmdbuf cmds[] = {
+	static uint8_t ack;
+	static struct dfu_cmdbuf cmds[] = {
 		{
 			.dir = OUT,
-			.buf.out = cmdb,
+			.buf = {
+				.out = cmdb,
+			},
 			.len = sizeof(cmdb),
 		},
 		{
 			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
+		{
+			.dir = IN,
 			.len = sizeof(*r),
+			.timeout = 300,
 		},
 	};
-	static struct stm32_usart_cmdescr descr = {
+	static struct dfu_cmddescr descr0 = {
 		.cmdbufs = cmds,
 		.ncmdbufs = ARRAY_SIZE(cmds),
 	};
 
-	cmds[1].buf.in = r;
-	return send_cmd(target, &descr);
+	descr0.state = &priv->cmd_state;
+	descr0.timeout = &priv->cmd_timeout;
+	cmds[2].buf.in = r;
+	priv->curr_descr = &descr0;
+	return dfu_cmd_do_sync(target, &descr0);
 }
 
 static int gid_cmd(struct dfu_target *target, struct stm32_gid_cmd_reply *r)
 {
+	struct stm32_usart_data *priv = target->priv;
 	static const uint8_t cmdb[] = { 0x02, 0xfd, };
-	static struct stm32_usart_cmdbuf cmds[] = {
+	static uint8_t ack;
+	static struct dfu_cmdbuf cmds[] = {
 		{
 			.dir = OUT,
-			.buf.out = cmdb,
+			.buf = {
+				.out = cmdb,
+			},
 			.len = sizeof(cmdb),
 		},
 		{
 			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
+		{
+			.dir = IN,
 			.len = sizeof(*r),
+			.timeout = 300,
 		},
 	};
-	static struct stm32_usart_cmdescr descr = {
+	static struct dfu_cmddescr descr0 = {
 		.cmdbufs = cmds,
 		.ncmdbufs = ARRAY_SIZE(cmds),
 	};
 
-	cmds[1].buf.in = r;
-	return send_cmd(target, &descr);
+	cmds[2].buf.in = r;
+	descr0.state = &priv->cmd_state;
+	descr0.timeout = &priv->cmd_timeout;
+	priv->curr_descr = &descr0;
+	return dfu_cmd_do_sync(target, &descr0);
 }
 
 int stm32_usart_init(struct dfu_target *target,
 		     struct dfu_interface *interface)
 {
+	static struct stm32_usart_data priv;
 	target->interface = interface;
-	dfu_log("%s: target = %p, interface = %p\n", __func__,
-		target, interface);
+	memset(&priv, 0, sizeof(priv));
+	target->priv = &priv;
+	dfu_log("STM32-USART target initialized\n");
 	return 0;
 }
 
@@ -190,6 +157,7 @@ int stm32_usart_probe(struct dfu_target *target)
 	int stat, i;
 	struct stm32_get_cmd_reply r1;
 	struct stm32_gid_cmd_reply r2;
+	struct stm32_usart_data *priv = target->priv;
 
 	dfu_log("Probing stm32 target\n");
 	stat = get_cmd(target, &r1);
@@ -201,11 +169,11 @@ int stm32_usart_probe(struct dfu_target *target)
 		dfu_log("\t0x%02x\n", r1.supported_commands[i]);
 	if (r1.supported_commands[6] == 0x44) {
 		dfu_log("Extended memory erase cmd supported\n");
-		target_flags |= STM32_EXTENDED_MEMORY_ERASE;
+		priv->target_flags |= STM32_EXTENDED_MEMORY_ERASE;
 	}
 	if (r1.bootloader_version == 0x31) {
 		dfu_log("Double NAK quirk enabled\n");
-		target_flags |= STM32_DOUBLE_NAK;
+		priv->target_flags |= STM32_DOUBLE_NAK;
 	}
 	stat = gid_cmd(target, &r2);
 	if (stat < 0)
@@ -215,132 +183,265 @@ int stm32_usart_probe(struct dfu_target *target)
 	return 0;
 }
 
+static void _chunk_done(struct dfu_target *target,
+			const struct dfu_cmddescr *descr)
+{
+	struct stm32_usart_data *priv = target->priv;
+
+	if (descr->state->status == DFU_CMD_STATUS_OK)
+		dfu_dbg("chunk 0x%08x programmed OK\n",
+			(unsigned int)priv->curr_chunk_addr);
+	dfu_binary_file_chunk_done(target->dfu->bf, priv->curr_chunk_addr,
+				   descr->state->status == DFU_CMD_STATUS_OK ?
+				   0 : -1);
+}
+
 /* Chunk of binary data is available for writing */
 int stm32_usart_chunk_available(struct dfu_target *target,
 				phys_addr_t address,
 				const void *buf, unsigned long sz)
 {
-	int stat;
-	unsigned long written;
 	static const uint8_t cmdb[] = { 0x31, 0xce, };
 	static uint32_t addr;
-	static int _nbytes;
-	static uint8_t nbytes;
-	static struct stm32_usart_cmdbuf cmds[] = {
-		{
+	static uint8_t nbytes, ack, checksum;
+	static struct dfu_cmdbuf cmds[] = {
+		/* Command, ~Command */
+		[0] = {
 			.dir = OUT,
-			.buf.out = cmdb,
+			.buf = {
+				.out = cmdb,
+			},
 			.len = sizeof(cmdb),
 		},
-		{
+		/* Wait for acknowledge */
+		[1] = {
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
+		/* Send address */
+		[2] = {
 			.dir = OUT,
-			.buf.out = &addr,
+			.buf = {
+				.out = &addr,
+			},
 			.flags = START_CHECKSUM|SEND_CHECKSUM,
 			.len = sizeof(addr),
 		},
-		{
+		/* Wait for acknowledge */
+		[3] = {
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
+		/* Send data */
+		[4] = {
 			.dir = OUT,
-			.flags = START_CHECKSUM|NO_ACK,
-			.buf.out = &nbytes,
+			.flags = START_CHECKSUM,
+			.buf = {
+				.out = &nbytes,
+			},
 			.len = sizeof(nbytes),
 		},
-		{
+		/* Send checksum */
+		[5] = {
 			.dir = OUT,
 			.flags = SEND_CHECKSUM,
 		},
+		/* Wait for acknowledge */
+		[6] = {
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
 	};
-	static struct stm32_usart_cmdescr descr = {
+	static struct dfu_cmddescr descr0 = {
 		.cmdbufs = cmds,
 		.ncmdbufs = ARRAY_SIZE(cmds),
+		.completed = _chunk_done,
+		.checksum_update = checksum_update,
+		.checksum_ptr = &checksum,
+		.checksum_size = sizeof(checksum),
 	};
+	struct stm32_usart_data *priv = target->priv;
 
-	for (written = 0; written < sz; ) {
-		_nbytes = min(sz - written - 1, 255);
-		nbytes = _nbytes;
-		addr = cpu_to_be32(address + written + 0x8000000);
-		dfu_dbg("_nbytes = %d, written = %lu, sz = %lu\n", _nbytes,
-			written, sz);
-		cmds[3].len = nbytes + 1;
-		cmds[3].buf.out = ((uint8_t *)buf) + written;
-		stat = send_cmd(target, &descr);
-		if (stat < 0)
-			return stat;
-		written += nbytes + 1;
-		dfu_log_noprefix(".");
+	if (sz > 256) {
+		dfu_err("%s: invalid length %lu\n", __func__, sz);
+		return -1;
 	}
-	return 0;
+	descr0.state = &priv->cmd_state;
+	descr0.timeout = &priv->cmd_timeout;
+	/* Asynchronous command */
+	priv->curr_chunk_addr = address;
+	priv->curr_descr = &descr0;
+	addr = cpu_to_be32(address);
+	nbytes = sz - 1;
+	cmds[5].len = sz;
+	cmds[5].buf.out = buf;
+	return dfu_cmd_start(target, &descr0);
+}
+
+static void _memory_erase_done(struct dfu_target *target,
+			       const struct dfu_cmddescr *descr)
+{
+	if (descr->state->status != DFU_CMD_STATUS_OK) {
+		dfu_err("ERROR ERASING MEMORY\n");
+		dfu_notify_error(target->dfu);
+		return;
+	}
+	dfu_dbg("Memory erased OK\n");
 }
 
 static int stm32_usart_target_erase_all(struct dfu_target *target)
 {
-	int stat;
 	static const uint8_t cmdb[] = { 0x44, 0xbb, };
 	static const uint8_t se[] = { 0xff, 0xff, };
-	static struct stm32_usart_cmdbuf cmds[] = {
+	static uint8_t ack, checksum;
+	static struct dfu_cmdbuf cmds[] = {
 		{
 			.dir = OUT,
-			.buf.out = cmdb,
+			.buf = {
+				.out = cmdb,
+			},
 			.len = sizeof(cmdb),
 		},
 		{
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 200,
+			.completed = _check_ack,
+		},
+		{
 			.dir = OUT,
-			.buf.out = se,
+			.buf = {
+				.out = se,
+			},
 			.flags = START_CHECKSUM|SEND_CHECKSUM,
 			.len = sizeof(se),
 		},
+		{
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 60000,
+			.completed = _check_ack,
+		},
 	};
-	static struct stm32_usart_cmdescr descr = {
+	static struct dfu_cmddescr descr0 = {
 		.cmdbufs = cmds,
 		.ncmdbufs = ARRAY_SIZE(cmds),
+		.checksum_ptr = &checksum,
+		.checksum_size = sizeof(checksum),
+		.checksum_update = checksum_update,
+		.completed = _memory_erase_done,
 	};
-	dfu_log("Erasing memory\n");
-	stat = send_cmd(target, &descr);
-	if (stat < 0)
-		return stat;
-	dfu_log("Memory erased\n");
-	return stat;
+	struct stm32_usart_data *priv = target->priv;
+
+	dfu_log("Starting global memory erase\n");
+	descr0.state = &priv->cmd_state;
+	descr0.timeout = &priv->cmd_timeout;
+	priv->curr_descr = &descr0;
+	return dfu_cmd_start(target, &descr0);
 }
 
 /* Reset and sync target */
 static int stm32_usart_reset_and_sync(struct dfu_target *target)
 {
-	char c = 0x7f, rpy;
+	struct stm32_usart_data *priv = target->priv;
+	struct dfu_interface *interface = target->interface;
 	int stat = 0, i;
+	static uint8_t cmdb[] = { 0x7f, };
+	static uint8_t ack;
+	static struct dfu_cmdbuf cmds[] = {
+		{
+			.dir = OUT,
+			.buf = {
+				.out = cmdb,
+			},
+			.len = sizeof(cmdb),
+		},
+		{
+			.dir = IN,
+			.buf = {
+				.in = &ack,
+			},
+			.len = sizeof(ack),
+			.timeout = 100,
+			.completed = _check_ack,
+		},
+	};
+	static struct dfu_cmddescr descr0 = {
+		.cmdbufs = cmds,
+		.ncmdbufs = ARRAY_SIZE(cmds),
+	};
 
-	/* Reset and sync: hw reset and enter bootloader */
-	if (target->interface->ops->target_reset)
-		stat = target->interface->ops->target_reset(target->interface);
-	if (stat < 0)
-		return stat;
-	if (!target->interface->ops->write) {
-		dfu_err("interface has no write operation\n");
-		return -1;
-	}
-	for (i = 0; i < 1; i++) {
-		stat = target->interface->ops->write(target->interface, &c,
-						     sizeof(c));
+	descr0.state = &priv->cmd_state;
+	descr0.timeout = &priv->cmd_timeout;
+	for (i = 0; i < 5; i++) {
+		/* Reset and sync: hw reset and enter bootloader */
+		if (interface->ops->target_reset)
+			stat = interface->ops->target_reset(interface);
 		if (stat < 0)
 			return stat;
-		stat = target->interface->ops->read(target->interface, &rpy,
-						    sizeof(rpy));
-		if (stat < 1)
-			continue;
-		if (rpy == 0x79)
-			break;
+		priv->curr_descr = &descr0;
+		if (!dfu_cmd_do_sync(target, &descr0)) {
+			dfu_dbg("Target sync OK\n");
+			return 0;
+		}
 	}
-	return 0;
+	dfu_err("Could not sync target\n");
+	return -1;
 }
 
-/* Let target run */
+/* Let the target run */
 int stm32_usart_run(struct dfu_target *target)
 {
+	struct dfu_interface *interface = target->interface;
+
+	if (interface->ops->target_run)
+		return interface->ops->target_run(interface);
+	if (interface->ops->target_reset)
+		return interface->ops->target_reset(interface);
 	return -1;
 }
 
-/* Interface event */
+/* Interface event: no async commands, do nothing */
 int stm32_usart_on_interface_event(struct dfu_target *target)
 {
-	return -1;
+	struct stm32_usart_data *priv = target->priv;
+
+	return dfu_cmd_on_interface_event(target, priv->curr_descr);
+}
+
+int stm32_usart_on_idle(struct dfu_target *target)
+{
+	struct stm32_usart_data *priv = target->priv;
+
+	if (!priv->curr_descr)
+		return 0;
+	return dfu_cmd_on_idle(target, priv->curr_descr);
+}
+
+static int stm32_usart_get_write_chunk_size(struct dfu_target *target)
+{
+	return 256;
 }
 
 struct dfu_target_ops stm32_dfu_target_ops = {
@@ -351,4 +452,6 @@ struct dfu_target_ops stm32_dfu_target_ops = {
 	.erase_all = stm32_usart_target_erase_all,
 	.run = stm32_usart_run,
 	.on_interface_event = stm32_usart_on_interface_event,
+	.on_idle = stm32_usart_on_idle,
+	.get_write_chunk_size = stm32_usart_get_write_chunk_size,
 };
