cdca2b35a006a2a3cc2810e74684cee0bc81eba6
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-01-19 16:18:03 +0100

    LINUX: add timestamp to log printouts

--- include/dfu-linux.h
+++ include/dfu-linux.h
@@ -7,6 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 
 struct linux_event_data {
@@ -14,13 +15,22 @@ struct linux_event_data {
 	int events;
 };
 
+static inline unsigned get_time(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
 extern const struct dfu_interface_ops linux_serial_stm32_interface_ops;
 extern const struct dfu_interface_ops linux_serial_arduino_uno_interface_ops;
 extern const struct dfu_interface_ops linux_spi_bp_nordic_target_interface_ops;
 extern const struct dfu_host_ops linux_dfu_host_ops;
 
-#define dfu_log(a,args...) fprintf(stderr, "DFU: " a, ##args)
-#define dfu_err(a,args...) fprintf(stderr, "DFU ERROR: " a, ##args)
+#define dfu_log(a,args...) fprintf(stderr, "[%08u] DFU: " a, get_time(), ##args)
+#define dfu_err(a,args...) fprintf(stderr, "[%08u] DFU ERROR: " a, get_time(), \
+				   ##args)
 #define dfu_log_noprefix(a,args...) fprintf(stderr, a, ##args)
 
 #include <string.h>
