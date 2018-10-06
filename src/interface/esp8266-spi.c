8f7895d27ad730d70005d652a14628e84feae8cb
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-10-06 23:30:00 +0200

    esp8266, spi: move registers definitions to esp8266-spi.c
    It is not necessary keeping them in a header file. Well, actually
    it is dangerous, since there are conflicting defines in arduino some headers.
    So let's keep everything as private as possible ....

--- src/interface/esp8266-spi.c
+++ src/interface/esp8266-spi.c
@@ -10,6 +10,33 @@
 #include "dfu-internal.h"
 #include "esp8266-spi.h"
 
+#define REG_SPI_BASE(i)		(0x60000200 - (i << 8))
+
+/* Registers' offset */
+#define SPI_CMD		0x00
+#  define SPI_BUSY	BIT(18)
+
+#define SPI_ADDR	0x04
+#define SPI_CTRL	0x08
+#define SPI_RD_STATUS	0x10
+#define SPI_CTRL2	0x14
+#define SPI_CLOCK	0x18
+#  define SPI_CLOCK_PRESCALER_SHIFT 18
+#  define SPI_CLOCK_DIVIDER_SHIFT 12
+#define SPI_USER	0x1c
+#define SPI_USER1	0x20
+#  define MOSI_BITLEN_MASK 0x1ff
+#  define MOSI_BITLEN_SHIFT 17
+#  define MISO_BITLEN_MASK 0x1ff
+#  define MISO_BITLEN_SHIFT 8
+
+#define SPI_USER2	0x24
+#define SPI_PIN		0x2c
+#define SPI_SLAVE	0x30
+#  define SLAVE_MODE	BIT(30)
+
+#define SPI_W(j)	(0x40 + ((j) << 2))
+
 /* These depend on the sdk we're building for :-( */
 #ifndef FUNC_HSPIQ_MISO
 #define FUNC_HSPIQ_MISO 2
